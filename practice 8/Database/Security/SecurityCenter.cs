6b5e570aa1e8e13d2498ddde57ac276eb2d710b4
Sorokovskys <Sorokovskys@ukr.net>
2024-10-25 08:34:33 +0300

    login

--- practice 8/Database/Security/SecurityCenter.cs
+++ practice 8/Database/Security/SecurityCenter.cs
@@ -1,12 +1,18 @@
+using Practice_8.Database.Exceptions;
+
 namespace Practice_8.Database.Security;
 
 public class SecurityCenter
 {
     private Repository<User> _users = new Repository<User>("users.dat");
 
-    public void Verify(User user)
+    public User login(string login, string password)
     {
-        
+        var foundUsers = _users.List.Where(x => x.Login.Equals(login));
+        if (!foundUsers.Any()) throw new UserNotFoundException();
+        var withCorrectPasswords = foundUsers.Where(x => x.Password.Equals(password));
+        if(!withCorrectPasswords.Any()) throw new InvalidPasswordException();
+        return withCorrectPasswords.First();
     }
 
     public SecurityCenter()
