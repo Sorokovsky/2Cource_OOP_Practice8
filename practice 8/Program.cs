a767e0d3036291359ce914b42bd52f78567e0f70
Sorokovsky <Sorokovskys@ukr.net>
2024-11-04 22:13:09 +0200

    refactor

--- practice 8/Program.cs
+++ practice 8/Program.cs
@@ -26,16 +26,19 @@ public static class Program
     private static CommandContext PrepareCommands()
     {
         var database = new DbContext();
-        CommandContext context = new(database, "Main menu", UserType.Create(Roles.Quest));
+        CommandContext context = new(database, "Main menu.", UserType.Create(Roles.Quest));
         context.AddCommand(new ExitCommand());
-        var usersContext = new CommandContext(database, "Users menu", UserType.Create(Roles.Quest));
+        var usersContext = new CommandContext(database, "Users menu.", UserType.Create(Roles.Quest));
         usersContext.AddCommand(new ExitCommand());
         usersContext.AddCommand(new RegisterCommand());
         usersContext.AddCommand(new LoginCommand());
         usersContext.AddCommand(new ShowAccountCommand());
         usersContext.AddCommand(new LogoutCommand());
+        var stadiumTypesContext = new CommandContext(database, "Stadium types menu.", UserType.Create(Roles.User));
+        stadiumTypesContext.AddCommand(new ExitCommand());
         usersContext.AddCommand(new CreateStadiumTypeCommand());
         context.AddCommand(usersContext);
+        context.AddCommand(stadiumTypesContext);
         return context;
     }
 }
--- practice 8/obj/Debug/net8.0/practice 8.AssemblyInfo.cs
+++ practice 8/obj/Debug/net8.0/practice 8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice 8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388f2638b5caa74bbe6c149a15d9233f1b1dfe24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f32b41d45a57f5e1743e322d4834bacb7d93631")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice 8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice 8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
